ec5e5151061bcd4f013b4a36979f7ebcd4017317
andrey <2baditsme@gmail.com>
2017-12-07 14:27:05 +0300

    testing sample code

---
+++ app/src/main/java/meme2lz/wumf/AlarmManagement.java
@@ -0,0 +1,71 @@
+package meme2lz.wumf;
+
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PowerManager;
+import android.widget.Toast;
+
+
+public class AlarmManagement extends BroadcastReceiver{
+
+    final public static String ONE_TIME="onetime";
+
+    @Override
+    public void onReceive(Context context, Intent intent){
+        PowerManager pm=(PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wl= pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,"YOUR TAG");
+//Осуществляем блокировку
+        wl.acquire();
+
+//Здесь можно делать обработку.
+        Bundle extras= intent.getExtras();
+        StringBuilder msgStr=new StringBuilder();
+
+        if(extras!=null && extras.getBoolean(ONE_TIME, Boolean.FALSE)){
+//проверяем параметр ONE_TIME, если это одиночный будильник,
+//выводим соответствующее сообщение.
+            msgStr.append("Одноразовый будильник: ");
+        }
+        Format formatter=new SimpleDateFormat("hh:mm:ss a");
+        msgStr.append(formatter.format(new Date()));
+
+        Toast.makeText(context, msgStr, Toast.LENGTH_LONG).show();
+
+//Разблокируем поток.
+        wl.release();
+    }
+
+    public void SetAlarm(Context context)
+    {
+        android.app.AlarmManager am=(android.app.AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent=new Intent(context, AlarmManagement.class);
+        intent.putExtra(ONE_TIME, Boolean.FALSE);//Задаем параметр интента
+        PendingIntent pi= PendingIntent.getBroadcast(context,0, intent,0);
+//Устанавливаем интервал срабатывания в 5 секунд.
+        am.setRepeating(android.app.AlarmManager.RTC_WAKEUP,System.currentTimeMillis(),1000*5,pi);
+    }
+
+    public void CancelAlarm(Context context)
+    {
+        Intent intent=new Intent(context, AlarmManagement.class);
+        PendingIntent sender= PendingIntent.getBroadcast(context,0, intent,0);
+        android.app.AlarmManager alarmManager=(android.app.AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancel(sender);//Отменяем будильник, связанный с интентом данного класса
+    }
+
+    public void setOnetimeTimer(Context context){
+        android.app.AlarmManager am=(android.app.AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent=new Intent(context, AlarmManagement.class);
+        intent.putExtra(ONE_TIME, Boolean.TRUE);//Задаем параметр интента
+        PendingIntent pi= PendingIntent.getBroadcast(context,0, intent,0);
+        am.set(android.app.AlarmManager.RTC_WAKEUP,System.currentTimeMillis(),pi);
+    }
+
+}
--- app/src/main/java/meme2lz/wumf/MainActivity.java
+++ app/src/main/java/meme2lz/wumf/MainActivity.java
@@ -1,13 +1,76 @@
+//package meme2lz.wumf;
+//
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//}
+
+
 package meme2lz.wumf;
-
 import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-
+import android.app.Activity;
+import android.content.Context;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import android.support.v4.app.NavUtils;
 public class MainActivity extends AppCompatActivity {
 
+    private AlarmManagement alarm;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        alarm = new AlarmManagement();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    public void startRepeatingTimer(View view) {
+        Context context = this.getApplicationContext();
+        if (alarm != null) {
+            alarm.SetAlarm(context);
+        } else {
+            Toast.makeText(context, "Alarm is null", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void cancelRepeatingTimer(View view) {
+        Context context = this.getApplicationContext();
+        if (alarm != null) {
+            alarm.CancelAlarm(context);
+        } else {
+            Toast.makeText(context, "Alarm is null", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onetimeTimer(View view) {
+        Context context = this.getApplicationContext();
+        if (alarm != null) {
+            alarm.setOnetimeTimer(context);
+        } else {
+            Toast.makeText(context, "Alarm is null", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.activity_widget_alarm_manager, menu);
+//        return true;
+//    }
 }
+
